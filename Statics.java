696b7364b4a65c0619ab26d688f68670a2afb34e
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-14 20:29:33 +0800

    Add files via upload
    The Statics and out of Ketwords in cpp

---
+++ Statics.java
@@ -0,0 +1,21 @@
+package tank;
+
+public class Statics {
+  public static void countofcpp(String[] words,String text)
+  { int totalnum=0;
+	  for(int i=0;i<words.length;i++)
+	{   String word=words[i];
+    int count=0;
+    String copytext=text;
+	while(copytext.contains(words[i]))
+	{
+		count++;
+		int index = copytext.indexOf(word);	
+		copytext = copytext.substring(index + word.length());
+		totalnum++;
+	}
+	System.out.printf("%s num: %d\n",words[i],count);
+    }
+	  System.out.printf("total num: %d\n",totalnum);
+  }
+}

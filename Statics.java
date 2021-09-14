9083ee5247bacbeb7708b53ca769f34dc267cb61
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-14 20:40:54 +0800

    Update Statics.java

--- Statics.java
+++ Statics.java
@@ -1,22 +1,36 @@
-package tank;
+package keyword;
 
 public class Statics {
-  public static void countofcpp(String[] words,String text)
-  { int totalnum=0;
-    for(int i=0;i<words.length;i++)
-     {   String word=words[i];
-        int count=0;
-        String copytext=text;
-	while(copytext.contains(words[i]))
-	{
-		count++;
-		int index = copytext.indexOf(word);	
-		copytext = copytext.substring(index + word.length());
-		totalnum++;
-	}
-	System.out.printf("%s num: %d\n",words[i],count);
-     }
-	  System.out.printf("total num: %d\n",totalnum);
- }
+	  public static void countofcpp(String[] words,String text)
+	  { int totalnum=0;
+		  for(int i=0;i<words.length;i++)
+	   {   String word=words[i];
+	    int count=0;
+	    String copytext=text;
+		while(copytext.contains(words[i]))
+		{ if(words[i].equals("do"))
+		 {
+			int index = copytext.indexOf(word);
+			if (copytext.charAt(index+2)!='u')
+			{
+				count++;
+				copytext = copytext.substring(index + word.length());
+				totalnum++;
+			}
+			else
+			{
+				copytext = copytext.substring(index + word.length());
+			}
+		 }
+		 else
+		 {	count++;
+			int index = copytext.indexOf(word);	
+			copytext = copytext.substring(index + word.length());
+			totalnum++;
+		 }	
+		}
+		if(count>0) System.out.printf("%s num: %d\n",words[i],count);
+	   }
+		  System.out.printf("total num: %d\n",totalnum);
+	  }
 }
-

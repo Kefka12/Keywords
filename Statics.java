dace03dd4eaf15aae3d4a648761c2e54624e164f
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-15 00:10:02 +0800

    统计被转换为字符串的CPP文档中的关键字并输出
    2.0版本 实现了输出switch case结构每组对应的case个数功能，同时使用HashMap存储关键字统计信息，方便后续操作。

--- Statics.java
+++ Statics.java
@@ -1,16 +1,25 @@
 package keyword;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Statics {
+	  
 	  public static void countofcpp(String[] words,String text)
-	  { int totalnum=0;
-            for(int i=0;i<words.length;i++)
-	   {   String word=words[i];
+	  { Map<String,Integer>map = new HashMap<>();
+	     int totalnum=0;
+		  ArrayList<Integer> list= new ArrayList<Integer>();
+		  //list.add(2);
+		  for(int i=0;i<words.length;i++)
+	   { String word=words[i];
 	    int count=0;
 	    String copytext=text;
 		while(copytext.contains(words[i]))
-		{ if(words[i].equals("do"))
+		{ int index = copytext.indexOf(word);
+			if(words[i].equals("do"))
 		 {
-			int index = copytext.indexOf(word);
+			
 			if (copytext.charAt(index+2)!='u')
 			{
 				count++;
@@ -24,13 +33,33 @@ public class Statics {
 		 }
 		 else
 		 {	count++;
-			int index = copytext.indexOf(word);	
 			copytext = copytext.substring(index + word.length());
 			totalnum++;
+			if(words[i].equals("switch"))
+			{Integer countofcase=Searchcases.search(copytext);
+			list.add(countofcase);
+			}
 		 }	
 		}
-		if(count>0) System.out.printf("%s num: %d\n",words[i],count);
+	    
+		if(count>0)
+		{   
+		    map.put(words[i],count);
+		} 
 	   }
 		  System.out.printf("total num: %d\n",totalnum);
+		  for (String i : map.keySet()) {
+			    if(i!="case")
+			    System.out.printf("%s num: %d\n",i,map.get(i));
+			    else
+			    {
+			    	System.out.printf("%s num:",i);
+			    	for(int k=0;k<list.size();k++)
+			    	{System.out.printf(" %d",list.get(k));
+			    	}
+			    	System.out.printf("\n");
+			    }
+	        }
+		  
 	  }
 }

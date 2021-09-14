b03cbda0bd77d847f0b2e3938f723a5884213b8b
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-15 00:03:04 +0800

    程序主干部分

--- Mainproject.java
+++ Mainproject.java
@@ -1,12 +1,12 @@
 package keyword;
 
-public class main {
-
+public class Mainproject {
+	
 	public static void main(String[] args) {
 		String path=null;
 		if(args.length==0) {path = "d:\\test.cpp";}
 		else path=args[0];
-        String text=FileToString.filetostring(path);
+                String text=FileToString.filetostring(path);
 		String[] keywords={"do","auto","break","case","char","const"
 				,"continue","default","double","else","enum"
 				,"extern","float","for","goto","if","int","long","register"
@@ -14,5 +14,6 @@ public class main {
 				,"typedef","union","unsigned","void","volatile","while"};
 		Statics.countofcpp(keywords, text);
 	}
+
 
 }

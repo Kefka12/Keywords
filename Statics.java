002ab57fba74f6fb174f8285a874cb6aef3217e2
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-14 20:37:00 +0800

    对于关键字的统计及统计结果输出
    目前只实现了基础要求1.0

--- Statics.java
+++ Statics.java
@@ -3,10 +3,10 @@ package tank;
 public class Statics {
   public static void countofcpp(String[] words,String text)
   { int totalnum=0;
-	  for(int i=0;i<words.length;i++)
-	{   String word=words[i];
-    int count=0;
-    String copytext=text;
+    for(int i=0;i<words.length;i++)
+     {   String word=words[i];
+        int count=0;
+        String copytext=text;
 	while(copytext.contains(words[i]))
 	{
 		count++;
@@ -15,7 +15,8 @@ public class Statics {
 		totalnum++;
 	}
 	System.out.printf("%s num: %d\n",words[i],count);
-    }
+     }
 	  System.out.printf("total num: %d\n",totalnum);
-  }
+ }
 }
+

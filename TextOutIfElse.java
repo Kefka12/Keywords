6aa9127131a9dd8ac67c51115aff3ffb33cec589
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-15 20:45:22 +0800

    输出文档中if-else和if-else-if结构个数的方法

--- TextOutIfElse.java
+++ TextOutIfElse.java
@@ -3,13 +3,13 @@ package keyword;
 
 public class TextOutIfElse {
 	public static void out1(int num1)
-	  {
+	 {
 		  System.out.printf("if-else num: %d\n",num1);
 		 
-	  }
+	 }
 	public static void out2(int num2)
-	  {
+	 {
 		  System.out.printf("if-elseif-else num: %d\n",num2);
 		 
-	  } 
+	 } 
 }

e607eb1ae4e82df21b45b1ae949561ef33d41606
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-15 20:40:50 +0800

    实现了在一组Switch结构中统计cases数的方法

--- SearchCases.java
+++ SearchCases.java
@@ -4,14 +4,14 @@ public class SearchCases {
    public static Integer search(String copytext)
    {
 	   int indexofendcase=copytext.indexOf("default");
-		Integer countofcase=0;			  
-         String temptext=copytext.substring(0,indexofendcase);
+           Integer countofcase=0;			  
+           String temptext=copytext.substring(0,indexofendcase);
 		 while(	JudgeOfKeywords.judgeif(temptext,"case"))
 		 {  int indexofcase=temptext.indexOf("case");
 			countofcase++;
 			temptext = temptext.substring(indexofcase + 4);
 		 }
        
-		return countofcase;
+	  return countofcase;
    }
 }

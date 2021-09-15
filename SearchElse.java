f11d8e48179f8f0347f81e9a047d7a2b6aa759ec
Kefka12 <68675899+Kefka12@users.noreply.github.com>
2021-09-15 20:41:33 +0800

    实现了从文段中搜索并提取else关键字位置的方法（主要为了与else-if中的else区分）

--- SearchElse.java
+++ SearchElse.java
@@ -9,7 +9,7 @@ public class SearchElse {
 			  String judges=copytext.substring(index, index+7);
 			  char judge2=' ';
 			  if(index>0) judge2=copytext.charAt(index-1);
-		      if((judge>='a'&&judge<='z')||(judge>='A'&&judge<='Z')||(judge>='0'&&judge<='9')||(judge=='_')||(judges=="else if")
+		          if((judge>='a'&&judge<='z')||(judge>='A'&&judge<='Z')||(judge>='0'&&judge<='9')||(judge=='_')||(judges=="else if")
 		    		  ||(judge2>='a'&&judge2<='z')||(judge2>='A'&&judge2<='Z')||(judge2>='0'&&judge2<='9')||(judge2=='_'))
 	        {   copytext = copytext.substring(index + 4);
 			    continue;
